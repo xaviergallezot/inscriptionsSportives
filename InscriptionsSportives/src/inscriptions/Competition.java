8c469fd8fc5b0769ff58c1b28800bca6d85764a5
Klaus <alexandre.mesle@gmail.com>
2016-01-15 00:37:19 +0100

    * premier commit

---
+++ InscriptionsSportives/src/inscriptions/Competition.java
@@ -0,0 +1,151 @@
+package inscriptions;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Représente une compétition, c'est-à-dire un ensemble de candidats 
+ * inscrits à un événement, les inscriptions sont closes à la date dateCloture.
+ *
+ */
+
+public class Competition implements Comparable<Competition>, Serializable
+{
+	private static final long serialVersionUID = -2882150118573759729L;
+	private Inscriptions inscriptions;
+	private String nom;
+	private Set<Candidat> candidats;
+	private Date dateCloture;
+	private boolean enEquipe = false;
+
+	Competition(Inscriptions inscriptions, String nom, Date dateCloture, boolean enEquipe)
+	{
+		this.enEquipe = enEquipe;
+		this.inscriptions = inscriptions;
+		this.nom = nom;
+		this.dateCloture = dateCloture;
+		candidats = new TreeSet<>();
+	}
+	
+	/**
+	 * Retourne le nom de la compétition.
+	 * @return
+	 */
+	
+	public String getNom()
+	{
+		return nom;
+	}
+	
+	/**
+	 * Retourne la date de cloture des inscriptions.
+	 * @return
+	 */
+	
+	public Date getDateCloture()
+	{
+		return dateCloture;
+	}
+	
+	/**
+	 * Est vrai si et seulement si les inscriptions sont réservées aux équipes.
+	 * @return
+	 */
+	
+	public boolean estEnEquipe()
+	{
+		return enEquipe;
+	}
+	
+	/**
+	 * Modifie la date de cloture des inscriptions. Il est possible de la reculer 
+	 * mais pas de l'avancer.
+	 * @param dateCloture
+	 */
+	
+	public void setDateCloture(Date dateCloture)
+	{
+		// TODO vérifier que l'on avance pas la date.
+		this.dateCloture = dateCloture;
+	}
+	
+	/**
+	 * Retourne l'ensemble des candidats inscrits.
+	 * @return
+	 */
+	
+	public Set<Candidat> getCandidats()
+	{
+		return Collections.unmodifiableSet(candidats);
+	}
+	
+	/**
+	 * Inscrit un candidat de type Personne à la compétition. Provoque une
+	 * exception si la compétition est réservée aux équipes.
+	 * @param personne
+	 * @return
+	 */
+	
+	public boolean add(Personne personne)
+	{
+		// TODO vérifier que la date de clôture n'est pas passée
+		if (enEquipe)
+			throw new RuntimeException();
+		personne.add(this);
+		return candidats.add(personne);
+	}
+
+	/**
+	 * Inscrit un candidat de type Equipe à la compétition. Provoque une
+	 * exception si la compétition est réservée aux personnes.
+	 * @param personne
+	 * @return
+	 */
+
+	public boolean add(Equipe equipe)
+	{
+		// TODO vérifier que la date de clôture n'est pas passée
+		if (!enEquipe)
+			throw new RuntimeException();
+		equipe.add(this);
+		return candidats.add(equipe);
+	}
+
+	/**
+	 * Désinscrit un candidat.
+	 * @param candidat
+	 * @return
+	 */
+	
+	public boolean remove(Candidat candidat)
+	{
+		candidat.remove(this);
+		return candidats.remove(candidat);
+	}
+	
+	/**
+	 * Supprime la compétition de l'application.
+	 */
+	
+	public void delete()
+	{
+		for (Candidat candidat : candidats)
+			remove(candidat);
+		inscriptions.remove(this);
+	}
+	
+	@Override
+	public int compareTo(Competition o)
+	{
+		return getNom().compareTo(o.getNom());
+	}
+	
+	@Override
+	public String toString()
+	{
+		return getNom();
+	}
+}
---
+++ InscriptionsSportives/src/inscriptions/Inscriptions.java
@@ -0,0 +1,212 @@
+package inscriptions;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Date;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Point d'entrée dans l'application, un seul objet de type Inscription
+ * permet de gérer les compétitions, candidats (de type equipe ou personne)
+ * ainsi que d'inscrire des candidats à des compétition.
+ */
+
+public class Inscriptions implements Serializable
+{
+	private static final long serialVersionUID = -3095339436048473524L;
+	private static final String FILE_NAME = "Inscriptions.srz";
+	private static Inscriptions inscriptions;
+	
+	private SortedSet<Competition> competitions = new TreeSet<>();
+	private SortedSet<Candidat> candidats = new TreeSet<>();
+
+	private Inscriptions()
+	{
+	}
+	
+	/**
+	 * Retourne les compétitions.
+	 * @return
+	 */
+	
+	public SortedSet<Competition> getCompetitions()
+	{
+		return Collections.unmodifiableSortedSet(competitions);
+	}
+	
+	/**
+	 * Retourne tous les candidats (personnes et équipes confondues).
+	 * @return
+	 */
+	
+	public SortedSet<Candidat> getCandidats()
+	{
+		return Collections.unmodifiableSortedSet(candidats);
+	}
+
+	/**
+	 * Créée une compétition. Ceci est le seul moyen, il n'y a pas
+	 * de constructeur public dans {@link Competition}.
+	 * @param nom
+	 * @param dateCloture
+	 * @param enEquipe
+	 * @return
+	 */
+	
+	public Competition createCompetition(String nom, 
+			Date dateCloture, boolean enEquipe)
+	{
+		Competition competition = new Competition(this, nom, dateCloture, enEquipe);
+		competitions.add(competition);
+		return competition;
+	}
+
+	/**
+	 * Créée une Candidat de type Personne. Ceci est le seul moyen, il n'y a pas
+	 * de constructeur public dans {@link Personne}.
+
+	 * @param nom
+	 * @param prenom
+	 * @param mail
+	 * @return
+	 */
+	
+	public Personne createPersonne(String nom, String prenom, String mail)
+	{
+		Personne personne = new Personne(this, nom, prenom, mail);
+		candidats.add(personne);
+		return personne;
+	}
+	
+	/**
+	 * Créée une Candidat de type équipe. Ceci est le seul moyen, il n'y a pas
+	 * de constructeur public dans {@link Equipe}.
+	 * @param nom
+	 * @param prenom
+	 * @param mail
+	 * @return
+	 */
+	
+	public Equipe createEquipe(String nom, Personne entraineur)
+	{
+		Equipe equipe = new Equipe(this, nom);
+		candidats.add(equipe);
+		return equipe;
+	}
+	
+	void remove(Competition competition)
+	{
+		competitions.remove(competition);
+	}
+	
+	void remove(Candidat candidat)
+	{
+		candidats.remove(candidat);
+	}
+	
+	/**
+	 * Retourne l'unique instance de cette classe.
+	 * Crée cet objet s'il n'existe déjà.
+	 * @return l'unique objet de type {@link Inscriptions}.
+	 */
+	
+	public static Inscriptions getInscriptions()
+	{
+		
+		if (inscriptions == null)
+		{
+			inscriptions = readObject();
+			if (inscriptions == null)
+				inscriptions = new Inscriptions();
+		}
+		return inscriptions;
+	}
+
+	
+	private static Inscriptions readObject()
+	{
+		ObjectInputStream ois = null;
+		try
+		{
+			FileInputStream fis = new FileInputStream(FILE_NAME);
+			ois = new ObjectInputStream(fis);
+			return (Inscriptions)(ois.readObject());
+		}
+		catch (IOException | ClassNotFoundException e)
+		{
+			return null;
+		}
+		finally
+		{
+				try
+				{
+					if (ois != null)
+						ois.close();
+				} 
+				catch (IOException e){}
+		}	
+	}
+	
+	/**
+	 * Sauvegarde le gestionnaire pour qu'il soit ouvert automatiquement 
+	 * lors d'une exécution ultérieure du programme.
+	 * @throws IOException 
+	 */
+	
+	public void sauvegarder() throws IOException
+	{
+		ObjectOutputStream oos = null;
+		try
+		{
+			FileOutputStream fis = new FileOutputStream(FILE_NAME);
+			oos = new ObjectOutputStream(fis);
+			oos.writeObject(this);
+		}
+		catch (IOException e)
+		{
+			throw e;
+		}
+		finally
+		{
+			try
+			{
+				if (oos != null)
+					oos.close();
+			} 
+			catch (IOException e){}
+		}
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "Candidats : " + getCandidats().toString()
+			+ "\nCompetitions  " + getCompetitions().toString();
+	}
+	
+	public static void main(String[] args)
+	{
+		Inscriptions inscriptions = Inscriptions.getInscriptions();
+		Competition flechettes = inscriptions.createCompetition("Mondial de fléchettes", null, false);
+		Personne tony = inscriptions.createPersonne("Tony", "Dent de plomb", "azerty"), 
+				boris = inscriptions.createPersonne("Boris", "le Hachoir", "ytreza");
+		flechettes.add(tony);
+		Equipe lesManouches = inscriptions.createEquipe("Les Manouches", boris);
+		lesManouches.delete();
+		System.out.println(inscriptions);
+		try
+		{
+			inscriptions.sauvegarder();
+		} 
+		catch (IOException e)
+		{
+			System.out.println("Sauvegarde impossible." + e);
+		}
+	}
+}
